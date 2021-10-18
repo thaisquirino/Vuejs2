2536ebf72edd423b04dc9626ccac4569bfee53e0
Thais <thaisuckeman@yahoo.com.br>
2021-10-18 15:59:15 -0300

    Componente  Filme

--- netflixvue/src/componentes/categoria.vue
+++ netflixvue/src/componentes/categoria.vue
@@ -6,76 +6,22 @@
       </span>
       <div ref="scroller" class="row">
         <div class="row__inner">
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img
-                class="gui-card__img"
-                src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"
-                alt=""
-              />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
+          <filme titulo="Outro Título"  imagem="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"></filme>
           </div>
-
- <div class="gui-card">
-            <div class="gui-card__media">
-              <img
-                class="gui-card__img"
-                src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"
-                alt=""
-              />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-           <div class="gui-card">
-            <div class="gui-card__media">
-              <img
-                class="gui-card__img"
-                src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"
-                alt=""
-              />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-           <div class="gui-card">
-            <div class="gui-card__media">
-              <img
-                class="gui-card__img"
-                src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700"
-                alt=""
-              />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
+         </div>
       <span v-on:mouseover="scrollDireita()" v-on:mouseout="clearScroll" class="handle handleNext active">
         <i class="fa fa-caret-right" aria-hidden="true"></i>
       </span>
+
     </div>
     </template>
      <script>
-        export default{
-        props:['titulo'],methods:{
+         import filme from './filme.vue';
+        export default{ 
+        props:['titulo'],
+        components:{
+            filme
+        },
             methods:{
             scrollDireita(){   
             this.intervalo=setInterval(() => {this.$refs.scroll.scrollLeft += 1},5);
